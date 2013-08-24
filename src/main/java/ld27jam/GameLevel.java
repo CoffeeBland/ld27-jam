2976cada2543d2e3a6706810164dfb3a793694ca
Frederic Gingras <frederic@gingras.cc>
2013-08-24 15:38:40 -0400

    Added Dungeon generator

--- src/main/java/ld27jam/GameLevel.java
+++ src/main/java/ld27jam/GameLevel.java
@@ -1,16 +1,37 @@
 package ld27jam;
 
+import ld27jam.entities.Dungeon;
+
 public class GameLevel 
 {
 	public int difficulty;
 	public int timeStage;
 	public int timeLeftToStage;
 	public int[] timeStageDurations = {1, 4, 8, 16, 32, 64, 128, 256, 512, 1024};
+	public Dungeon dungeon;
 	
 	public GameLevel(int level)
 	{
 		this.difficulty = level;
 		changeTimeStage(1);
+		
+		// Good now generate that maze
+		this.dungeon = new Dungeon();
+	}
+	
+	public int getHeight() 
+	{
+		return dungeon.ysize;
+	}
+	
+	public int getWidth() 
+	{
+		return dungeon.xsize;
+	}
+	
+	public int getCell(int x, int y)
+	{
+		return dungeon.getCell(x, y);
 	}
 	
 	public void changeTimeStage(int newTimeStage) 
