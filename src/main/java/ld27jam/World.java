2d96139766a764b41ad50e3f3f6fea3cdf713a4c
Frederic Gingras <frederic@gingras.cc>
2013-08-24 17:59:10 -0400

    Empty tiles are now empty

--- src/main/java/ld27jam/World.java
+++ src/main/java/ld27jam/World.java
@@ -83,11 +83,14 @@ public class World
 			for (int y = 0; y < ysize; y++) 
 			{
 				int tileType = gd.level.getCell(x, y);
-				if (tileType == gd.level.dungeon.tileDirtWall || 
-					tileType == gd.level.dungeon.tileUnused)
-					grid[x][y] = new Tile(TileType.Test2, x, y);
-				else
-					grid[x][y] = new Tile(TileType.Test, x, y);
+				if (tileType == gd.level.dungeon.tileDirtWall)
+				{
+				  grid[x][y] = new Tile(TileType.Test2, x, y);
+				}
+				else if (tileType != gd.level.dungeon.tileUnused)
+				{
+				  grid[x][y] = new Tile(TileType.Test, x, y);
+				}
 			}
 		}
 		character = new Entity(gd.level.dungeon.getClosestFreeCell(xsize / 2, xsize / 2), new Vector2f(0.7f, 0.7f), true, new Vector2f(-8, -30), new AnimatedSprite("res/sprites/tmpSheet.png", 48, 48, 8));
